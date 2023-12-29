efb6fd1828b07e3b14034fa91159127c2c8d2671
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2023-12-29 16:53:51 +0530

    React hook form : used to validate form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import YouTubeForm from './components/YouTubeForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YouTubeForm />
     </div>
   );
 }
---
+++ src/components/YouTubeForm.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
+let renderCount = 0;
+
+const YouTubeForm = () => {
+
+    //version - 1
+    // const form = useForm();
+
+    //version - 2 : With default values
+    const form = useForm({
+        defaultValues: {
+            username: "Sample",
+            email: "",
+            channel: "",
+            social: {
+                twitter: "",
+                facebook: ""
+            }
+        }
+    });
+
+    //version - 3 : with data from API
+    // const form = useForm({
+    //     defaultValues: async () => {
+    //         const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+    //         const data = await response.json();
+    //         return {
+    //             username: data.name,
+    //             email: data.email,
+    //             channel: ""
+    //         };
+    //     }
+    // })
+
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+
+    //This is one possible way to use register.
+    // const { name, ref, onChange, onBlur } = register("username")
+
+    const onSubmit = (data) => {
+        console.log("Form Submitted", data);
+    }
+
+    renderCount++;
+    return (
+        <div>
+            <h2>YouTube Form ({renderCount / 2})</h2>
+
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div className='form-control'>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        {...register("username", {
+                            required: "Username is required",
+                        })}
+                    />
+                    <p className='error'>{errors.username?.message}</p>
+                </div>
+
+                {/* <input
+                    type="text"
+                    id="username"
+                    name={name}
+                    ref={ref}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                /> */}
+
+                <div className='form-control'>
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        {...register("email", {
+                            required: {
+                                value: true,
+                                message: "Email is required"
+                            },
+                            pattern: {
+                                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                message: "Invalid email format"
+                            },
+                            // validate: (fieldValue) => {
+                            //     return fieldValue === "admin@example.com" ? "Enter a different email address" : true;
+                            // }
+                            validate: {
+                                notAdmin: (fieldValue) => {
+                                    return (
+                                        fieldValue === "admin@example.com" ? "Enter a different email address" : true
+                                    );
+                                },
+                                notBlackListed: (fieldValue) => {
+                                    return (
+                                        fieldValue.endsWith("baddomain.com") ? "This domain is not supported" : true
+                                    );
+                                }
+                            }
+                        })}
+                    />
+                    <p className='error'>{errors.email?.message}</p>
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="channel">Channel</label>
+                    <input
+                        type="text"
+                        id="channel"
+                        {...register("channel", {
+                            required: "Channel is required"
+                        })}
+                    />
+                    <p className='error'>{errors.channel?.message}</p>
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="twitter">Twitter</label>
+                    <input
+                        type="text"
+                        id="twitter"
+                        {...register("social.twitter")}
+                    />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="facebook">Facebook</label>
+                    <input
+                        type="text"
+                        id="facebook"
+                        {...register("social.facebook")}
+                    />
+                </div>
+
+                <button>Submit</button>
+            </form>
+            <DevTool control={control} />
+        </div>
+    )
+}
+
+export default YouTubeForm;
