d549d11796e9f8d00d5a0e26c3cf1b9d73522c49
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2023-12-30 10:47:28 +0530

    React hook form : Numeric and Date values

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -21,6 +21,8 @@ const YouTubeForm = () => {
             },
             phoneNumbers: ["", ""],
             phNumbers: [{ number: '' }],
+            age: 0,
+            dob: new Date()
         }
     });
 
@@ -174,9 +176,42 @@ const YouTubeForm = () => {
                                 }
                             </div>
                         ))}
-                        <button type='button' onClick={() => append({number: ""})}>Add phone number</button>
+                        <button type='button' onClick={() => append({ number: "" })}>Add phone number</button>
                     </div>
                 </div>
+
+                <div className='form-control'>
+                    <label htmlFor="age">Age</label>
+                    <input
+                        type="number"
+                        id="age"
+                        {...register("age", {
+                            valueAsNumber: true,
+                            required: {
+                                value: true,
+                                message: "Age is required"
+                            }
+                        })}
+                    />
+                    <p className='error'>{errors.age?.message}</p>
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor="dob">Date of birth</label>
+                    <input
+                        type="date"
+                        id="dob"
+                        {...register("dob", {
+                            valueAsDate: true,
+                            required: {
+                                value: true,
+                                message: "Date of birth is required"
+                            }
+                        })}
+                    />
+                    <p className='error'>{errors.dob?.message}</p>
+                </div>
+
                 <button>Submit</button>
             </form>
             <DevTool control={control} />
